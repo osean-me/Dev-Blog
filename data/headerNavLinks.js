0efc4de31c1d374b7a547ca4168fdf4fd4ffc003
SeongHeon Sim <deep@osean.me>
2023-02-26 16:15:50 +0900

    Edit Page Layout & Category

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,5 +1,7 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
+  { href: '/blog', title: 'All Posts' },
+  { href: '/life', title: 'Life' },
+  { href: '/programming', title: 'Programming' },
   { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -15,6 +15,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
   // If initialDisplayPosts exist, display it if no searchValue is specified
   const displayPosts =
     initialDisplayPosts.length > 0 && !searchValue ? initialDisplayPosts : filteredBlogPosts
+  const mainTitle = title === 'All Posts' ? 'blog' : title.toLowerCase()
 
   return (
     <>
@@ -63,7 +64,10 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
                   <div className="space-y-3 xl:col-span-3">
                     <div>
                       <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                        <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                        <Link
+                          href={`/blog/${mainTitle}/${slug}`}
+                          className="text-gray-900 dark:text-gray-100"
+                        >
                           {title}
                         </Link>
                       </h3>
@@ -84,7 +88,11 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
         </ul>
       </div>
       {pagination && pagination.totalPages > 1 && !searchValue && (
-        <Pagination currentPage={pagination.currentPage} totalPages={pagination.totalPages} />
+        <Pagination
+          currentPage={pagination.currentPage}
+          totalPages={pagination.totalPages}
+          path={mainTitle}
+        />
       )}
     </>
   )
